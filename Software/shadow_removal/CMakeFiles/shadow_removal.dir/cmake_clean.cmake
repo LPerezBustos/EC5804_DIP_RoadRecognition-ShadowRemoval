e9c1c3f3a41f0d327143c053461d1554274a1d1d
LPerezBustos <LuisAlejandro.2702@gmail.com>
2017-03-08 22:11:40 -0400

    some code

---
+++ Software/shadow_removal/CMakeFiles/shadow_removal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shadow_removal.dir/src/shadow_removal.cpp.o"
+  "bin/shadow_removal.pdb"
+  "bin/shadow_removal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shadow_removal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Software/shadow_removal/src/shadow_removal.cpp
+++ Software/shadow_removal/src/shadow_removal.cpp
@@ -9,16 +9,31 @@ using namespace cv;
 
 int main(){
 	//Nombre de la imagen que se va a cargar
-	char NombreImagen[] = "road_and_shadow.jpg";
+	char IMAGE_NAME[] = "road_and_shadow_2.jpg";
 
 	//Cargamos las imagenes y se comprueba que lo ha hecho correctamente
-	Mat src = imread(NombreImagen);
+	Mat src = imread(IMAGE_NAME);
 	if (!src.data){
 		cout << "Error al cargar la imagenes" << endl;
 		exit(1);
 	}
 
-	Mat dst;
+	/*vector<Mat> bgr_planes, H, S, V;
+	split(src, bgr_planes);
+
+   namedWindow("Original", CV_WINDOW_AUTOSIZE), imshow("Original", src);
+   namedWindow("BLUE PLANE", CV_WINDOW_AUTOSIZE), imshow("BLUE PLANE", bgr_planes[0]);
+   namedWindow("GREEN PLANE", CV_WINDOW_AUTOSIZE), imshow("GREEN PLANE", bgr_planes[1]);
+   namedWindow("RED PLANE", CV_WINDOW_AUTOSIZE), imshow("RED PLANE", bgr_planes[2]);*/
+   Mat hsv_image
+   for (int y = 0; y < src.cols; y++){
+       for(int x = 0; x < img.rows; x++){
+          image.at<cv::Vec3b>(y,x)[0]
+          uchar *blue = ((uchar*)(img->imageData + img->widthStep*y))[x*3];
+          uchar *green = ((uchar*)(img->imageData + img->widthStep*y))[x*3+1];
+          uchar *red = ((uchar*)(img->imageData + img->widthStep*y))[x*3+2];
+       }
+   }
 
 	cvWaitKey(0);
 	return 0;
