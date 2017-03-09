e6bcd7b2d1f43211402f4543338f93c9106649a9
LPerezBustos <LuisAlejandro.2702@gmail.com>
2017-03-08 20:51:59 -0400

    empezando a programar

--- Software/shadow_removal/README.md
+++ Software/shadow_removal/README.md
@@ -1,10 +1,6 @@
 Etapa de filtrado:  
 
-ENTRADA: Imagen RGB de vía con posibles sobras.
+ENTRADA: Imagen RGB de vía con posibles sombras.
 SALIDA:  Imagen RGB de vía con compensanción de sombras.
 
 RESPONSABLE: LPerezBustos
-
-***Solución basada en el paper***
-
-"Shadow Detection and Removal from Remote Sensing Images Using NDI and Morphological Operators" Singh et al
--- Software/shadow_removal/src/shadow_removal.cpp
+++ Software/shadow_removal/src/shadow_removal.cpp
@@ -1,57 +1,25 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
+#include <iostream>
 #include <stdio.h>
 
+using namespace std;
 using namespace cv;
 
-/** @function main */
-int main( int argc, char** argv )
-{
+int main(){
+	//Nombre de la imagen que se va a cargar
+	char NombreImagen[] = "road_and_shadow.jpg";
 
-  Mat src, src_gray;
-  Mat grad;
-  char* window_name = "Sobel Demo - Simple Edge Detector";
-  int scale = 1;
-  int delta = 0;
-  int ddepth = CV_16S;
+	//Cargamos las imagenes y se comprueba que lo ha hecho correctamente
+	Mat src = imread(NombreImagen);
+	if (!src.data){
+		cout << "Error al cargar la imagenes" << endl;
+		exit(1);
+	}
 
-  int c;
+	Mat dst;
 
-  /// Load an image
-  src = imread( argv[1] );
-
-  if( !src.data )
-  { return -1; }
-
-  GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-
-  /// Convert it to gray
-  cvtColor( src, src_gray, CV_BGR2GRAY );
-
-  /// Create window
-  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
-
-  /// Generate grad_x and grad_y
-  Mat grad_x, grad_y;
-  Mat abs_grad_x, abs_grad_y;
-
-  /// Gradient X
-  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_x, abs_grad_x );
-
-  /// Gradient Y
-  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_y, abs_grad_y );
-
-  /// Total Gradient (approximate)
-  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
-
-  imshow( window_name, grad );
-
-  waitKey(0);
-
-  return 0;
-  }
+	cvWaitKey(0);
+	return 0;
+}
